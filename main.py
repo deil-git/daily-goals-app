6ecdd0693ab73a9c1a16d6f722ba194ae129181d
deil-git <deil.enot@gmail.com>
2023-09-04 13:22:22 +0300

    Card and smooth animations

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ def main(page: ft.Page):
             progress_ring1.value = 1.0
             page.update()
             time.sleep(0.3)
-            pr.scale = 0.5
-            pr.transition = ft.AnimatedSwitcherTransition.SCALE
+            pr_icon.scale = 0
             page.update()
-            pr.content = done_icon
-            pr.scale = 1.0
+            pr_icon.content = done_icon
+            page.update()
+            time.sleep(0.6)
+            done_icon.scale = 0.85
+            pr_icon.scale = 0.85
             pl_b.opacity = 0.0
         else:
             animate(e)
@@ -60,7 +62,8 @@ def main(page: ft.Page):
         ft.icons.STAR_ROUNDED,
         size=100,
         tooltip='Молодец 🎉',
-        color=ft.colors.DEEP_PURPLE_200
+        color=ft.colors.DEEP_PURPLE_200,
+        scale=0,
     )
 
     plus_btn = ft.IconButton(
@@ -80,10 +83,10 @@ def main(page: ft.Page):
     pr_icon = ft.AnimatedSwitcher(
         goal_icon,
         transition=ft.AnimatedSwitcherTransition.FADE,
-        duration=700,
+        duration=1,
         switch_in_curve=ft.AnimationCurve.DECELERATE,
-        scale=ft.transform.Scale(scale=1),
-        animate_scale=ft.animation.Animation(600, ft.AnimationCurve.DECELERATE),
+        scale=ft.transform.Scale(scale=0.5),
+        animate_scale=ft.animation.Animation(1100, ft.AnimationCurve.EASE),
     )
 
     pl_b = ft.AnimatedSwitcher(
@@ -91,32 +94,49 @@ def main(page: ft.Page):
         animate_opacity=300,
     )
 
-    page.add(
-        ft.Column(
-            [
-                ft.Text("Коля круче всех!", style = "headlineSmall"),
-                ft.Stack(
-                    [
-                        ft.Container(
-                            content=progress_ring_background,
-                            alignment=ft.alignment.center,
-                        ),
-                        ft.Container(
-                            content=pr,
-                            alignment=ft.alignment.center,
-                        ),
-                        ft.Container(
-                            content=pr_icon,
-                            alignment=ft.alignment.center,
+    goal_text = ft.Text(
+        "Коля круче всех!",
+        style = "headlineSmall",
+        text_align=ft.TextAlign.CENTER
+    )
 
-                        ),
-                    ],
-                ),
-                pl_b,
-            ],
-            spacing=20,
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.add(
+        ft.Container(
+            ft.Column(
+                [
+                    goal_text,
+                    ft.Stack(
+                        [
+                            ft.Container(
+                                content=progress_ring_background,
+                                alignment=ft.alignment.center,
+                            ),
+                            ft.Container(
+                                content=pr,
+                                alignment=ft.alignment.center,
+                            ),
+                            ft.Container(
+                                content=pr_icon,
+                                alignment=ft.alignment.center,
+
+                            ),
+                        ],
+                    ),
+                    ft.Stack(
+                        [
+                            pl_b,
+                        ]
+                    ),
+
+                ],
+                spacing=20,
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            ),
+            bgcolor=ft.colors.BLACK,
+            width=180,
+            height=260,
+            border_radius=30,
         ),
     )
 
@@ -124,8 +144,8 @@ def main(page: ft.Page):
     progress_ring2.value = 0.0
     progress_ring_background.value = 1.0
     page.padding = 0
-    page.window_width = 400
-    page.window_height = 300
+    page.window_width = 700
+    page.window_height = 400
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.update()
