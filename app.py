67aba77715f43237114e37b41dd8064bcdf53157
deil-git <deil.enot@gmail.com>
2023-09-09 19:27:20 +0300

    GridView for goals

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ class App(ft.UserControl):
         self.settings_visible = False
         self.list_complete_goals_value = {}
         self.list_goals_value = {}
-        self.main_pr_value = ft.Text("0 %", weight=ft.FontWeight.W_600, width=70,
-                                     text_align=ft.TextAlign.END)
+        self.main_pr_text = ft.Text("0 %", weight=ft.FontWeight.W_600, width=70,
+                                    text_align=ft.TextAlign.END)
         self.main_pr = ft.ProgressBar(value=0, width=400, color=ft.colors.DEEP_PURPLE_200, bgcolor=ft.colors.WHITE)
-        self.goals = ft.Row(wrap=True)
+        self.goals = ft.GridView(height=530, width=415, child_aspect_ratio=0.8, spacing=10, padding=5, max_extent=260,)
 
         return ft.Column(
             [
                 ft.Row(
                     [
-                        self.main_pr_value,
+                        self.main_pr_text,
                         self.main_pr,
                         ft.IconButton(icon=ft.icons.SETTINGS, on_click=self.settings_clicked, width=30, height=30,
                                       icon_size=14, ),
@@ -27,14 +27,11 @@ class App(ft.UserControl):
                     alignment=ft.MainAxisAlignment.CENTER,
                     spacing=10
                 ),
-                ft.Container(
-                    self.goals,
-                    width=415,
-                    alignment=ft.alignment.center,
-                )
+                self.goals
             ],
             width=600,
-            alignment=ft.MainAxisAlignment.END,
+            height=630,
+            alignment=ft.MainAxisAlignment.START,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
 
@@ -55,24 +52,28 @@ class App(ft.UserControl):
                     self.goal_delete, self.update_main_pr, self.settings_visible)
         self.goals.controls.append(goal)
         self.list_goals_value[goal] = goal.goal_value
+        self.list_complete_goals_value[goal] = 0
         temp = int(round((sum(self.list_complete_goals_value.values()) / sum(self.list_goals_value.values())), 2) * 100)
         self.main_pr.value = temp / 100
-        self.main_pr_value.value = f"{temp} %"
+        self.main_pr_text.value = f"{temp} %"
         self.update()
 
     def goal_delete(self, goal):
-        print(self.list_goals_value)
-        self.goals.controls.remove(goal)
         self.list_goals_value.pop(goal)
         self.list_complete_goals_value.pop(goal)
-        temp = int(round((sum(self.list_complete_goals_value.values()) / sum(self.list_goals_value.values())), 2) * 100)
-        self.main_pr.value = temp / 100
-        self.main_pr_value.value = f"{temp} %"
+        self.goals.controls.remove(goal)
+        if sum(self.list_goals_value.values()) != 0:
+            temp = int(round((sum(self.list_complete_goals_value.values()) / sum(self.list_goals_value.values())), 2) * 100)
+            self.main_pr.value = temp / 100
+            self.main_pr_text.value = f"{temp} %"
+        else:
+            self.main_pr_text.value = f"0 %"
+            self.main_pr.value = 0
         self.update()
 
     def update_main_pr(self, goal):
         self.list_complete_goals_value[goal] = goal.goal_complete_value
         temp = int(round((sum(self.list_complete_goals_value.values()) / sum(self.list_goals_value.values())), 2) * 100)
         self.main_pr.value = temp / 100
-        self.main_pr_value.value = f"{temp} %"
+        self.main_pr_text.value = f"{temp} %"
         self.update()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main(page: ft.Page):
     page.title = "Daily goals"
     page.padding = 10
     page.window_width = 600
-    page.window_height = 400
+    page.window_height = 630
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.update()
